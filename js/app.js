d27ded03cc19116398aae1fd1c58e74446003759
rostahilse-lgtm <rostahilse@gmail.com>
2026-01-11 20:36:45 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -308,7 +308,7 @@ const app = Vue.createApp({
     resetApiUrl() {
       this.apiUrl = DEFAULT_API_URL;
       localStorage.setItem('apiUrl', DEFAULT_API_URL);
-      this.showMessage('✓ Obnoveno výchozí URL')
+      this.showMessage('✓ Obnoveno výchozí URL');
     }  
   },
                           
@@ -666,6 +666,7 @@ const app = Vue.createApp({
 
 app.use(Quasar);
 app.mount('#app');
+
 
 
 
