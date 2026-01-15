8a8a33c0e7136e63bea343f0792aa91e6e5690b2
rostahilse-lgtm <rostahilse@gmail.com>
2026-01-15 01:32:16 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Konfigurace API
-const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbyjfpwgcXuDjLOI2pvOl40rUB6Bks4GtQgS2iwlg04NgEBZxkh-eVbYH0GwUa4eSWOn/exec';
+const DEFAULT_API_URLhttps://script.google.com/macros/s/AKfycbx4hn-0M0ivPEx0Rx3B6FbU0znHx26l7APnzddyFEuAkGs1a6de5XiMnEa_rC3Cncxf/exec = '';
