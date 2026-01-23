776550c8a96d4fb1497a76e28690b3e2ffeef3af
rostahilse-lgtm <rostahilse@gmail.com>
2026-01-23 20:35:27 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbxmopQqNLpeVzoeg4nEVc1zfaote8WVoTpVc4EicACLHj-SfFlMIw9gtIdl48Qrxipq/exec';
+const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbxprYkHjm04c1gO3_vIdFXP8Wzf3n5lUsMK61eucvULlr12CT85vxc2FpChp0KLk-D_/exec';
