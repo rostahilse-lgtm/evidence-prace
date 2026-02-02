44ea3c75cabf17fb76167a72b5b1cbf5345f340d
rostahilse-lgtm <rostahilse@gmail.com>
2026-02-02 22:02:49 +0100

    Update admin.js nový s přidáním místa

--- js/components/admin/admin.js
+++ js/components/admin/admin.js
@@ -239,8 +239,8 @@ window.app.component('admin-component', {
           <div v-for="(record,idx) in selectedWorkerData.records" :key="idx" class="record-card">
             <div class="row items-center">
               <div class="col">
-                <div class="text-bold">{{ record[3] }}</div>
-                <div class="text-caption text-grey-7">{{ record[5] }}</div>
+                <div class="text-bold">{{ record[0] }}</div>
+                <div class="text-caption text-grey-7">{{ record[3] }} • {{ record[14] || 'Nezadáno' }}</div>
               </div>
               <div class="text-right">
                 <div class="text-bold text-primary">{{ record[7].toFixed(2) }} hod</div>
@@ -251,8 +251,8 @@ window.app.component('admin-component', {
             <div class="text-caption text-grey-7 q-mt-sm">
               {{ formatTimeRange(record[4], record[5]) }}
             </div>
-            <div v-if="record[11] > 0" class="text-caption text-orange q-mt-xs">
-              🚗 {{ record[11] }} km
+            <div v-if="record[12] > 0" class="text-caption text-orange q-mt-xs">
+              🚗 {{ record[12] }} km
             </div>
             <div v-if="record[8]" class="note-display">💬 {{ record[8] }}</div>
           </div>
