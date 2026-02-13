357a8444740c9a84d0cf0514c61e9b8b334cfce8
rostahilse-lgtm <rostahilse@gmail.com>
2026-02-13 07:06:33 +0100

    Update DEFAULT_API_URL to new endpoint nová imple

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbzFauK-XOZux-koM0rc-OyJZhQ-yjpLeVUY5VyzoCz--xvGLuUCJBhGh9czrIldKVa_/exec';
+const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbxlACadNIl6aWPmt0WKuCze2mZqK8QiJJNrhHxLvYKrL3KN-Qd0DLT08qswaFFApxdC/exec';
