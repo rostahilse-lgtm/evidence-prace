5629ade2eac65099dd7f1ffe47b1aa3f499c428a
rostahilse-lgtm <rostahilse@gmail.com>
2026-02-15 16:06:24 +0100

    Update DEFAULT_API_URL to new endpoint nový kod gs

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbxlACadNIl6aWPmt0WKuCze2mZqK8QiJJNrhHxLvYKrL3KN-Qd0DLT08qswaFFApxdC/exec';
+const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbzsRSiJ62fgE_6eCDI51Eg8sZWBA270aProUGULoNX8icwz9PBn66ABCegCiejSrzOP/exec';
