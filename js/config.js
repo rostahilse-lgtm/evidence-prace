beb9cbdb431508e096018d360fae54e7a6762927
rostahilse-lgtm <rostahilse@gmail.com>
2026-02-22 08:15:52 +0100

    Update DEFAULT_API_URL to new endpoint

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbzrFUG25rMX67__aJLAtSlccBdUhLQpQddNePG92t1dfBer4ualp-WVsXTv97-S-e0O/exec';
+const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbxmTfOD-kcemuRO6RAsJYsLJLwE-SQfNgP-2ntl-CQLbdOR50_Mkwq2AccHleOWdZRb/exec';
 
 const DEFAULT_CLOUD_SHIFT = true;
