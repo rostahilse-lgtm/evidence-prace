4031211cca5dd438f296ac5047bc83c05a279fd7
rostahilse-lgtm <rostahilse@gmail.com>
2026-02-22 23:22:53 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ window.app = Vue.createApp({
       const s = localStorage.getItem('dataSource') || 'new';
       if (s === 'history') return '· HIST';
       if (s === 'all') return '· VŠE';
-   return '· NOVÉ'; 
+   return ''; 
     }
   },
 
@@ -209,4 +209,5 @@ setTimeout(() => {
   window.app.use(Quasar);
   window.app.mount('#app');
 }, 100);
+
 
