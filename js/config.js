8fe82931a9c4baf865c57d3abd9cb83b048671b2
rostahilse-lgtm <rostahilse@gmail.com>
2026-02-25 19:57:01 +0100

    Update config.js nový kod dvě ceny oběda

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbzyocXCI3bpE15o0EyhzuQvruF3KeaXDdSbmerZeLgY6tgaxGo_lMuyeg_FHVBkDFE5/exec';
+const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbwze0wuMABepDAsOXmAKxS0GBvkXdOjaVvt7EUqW6g_VD3Oj2XfijiHlLVqAO74kF45/exec';
 
 const DEFAULT_CLOUD_SHIFT = true;
